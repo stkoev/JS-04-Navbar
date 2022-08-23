b1f690a8ea84e3fb7a3f2b7764518cbcddf11378
Stefan Koev <st.koev@outlook.com>
2022-08-23 22:10:59 +0300

    implement javascript

--- app.js
+++ app.js
@@ -3,3 +3,18 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+// navToggle.addEventListener("click", function (e) {
+//   links.classList.value.includes("show-links")
+//     ? links.classList.remove("show-links")
+//     : links.classList.add("show-links");
+//   console.log(links.classList.value);
+// });
+
+navToggle.addEventListener("click", function (e) {
+  links.classList.toggle("show-links");
+  console.log(links.classList.value);
+});
